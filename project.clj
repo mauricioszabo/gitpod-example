eb88e4a521dcc1eeb583e3d267308d87c440d1e7
Maurício Szabo <mauricio.szabo@gmail.com>
2020-05-03 19:12:38 -0300

    Initial commits

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject paprika "0.1.4"
+  :description "Simple utility functions"
+  :url "http://github.com/mauricioszabo/gitpod-example"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]])
